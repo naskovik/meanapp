99d52386b9baf0b38c34b31616cbf79a8b869a1d
naskovik <nazarskoreyko@gmail.com>
2020-04-06 13:24:00 +0200

    Finished

--- ejMean/src/app/edit-tarea/edit-tarea.component.ts
+++ ejMean/src/app/edit-tarea/edit-tarea.component.ts
@@ -15,6 +15,9 @@ export class EditTareaComponent implements OnInit {
   public tarea: TareaModel;
   public tareaEstadosSelect = [];
   public status: string;
+  public estado: string;
+  public titulo: string;
+  public fecha: Date;
 
   constructor(private route: ActivatedRoute, private router: Router, private tareaService: TareaService) { }
 
@@ -25,7 +28,13 @@ export class EditTareaComponent implements OnInit {
       let id = params.get('id');
       this.tareaService.getTarea(id).subscribe(
         response => {
-          if (response.tarea) this.tarea = response.tarea;
+          // if a tarea is correctly returned by backend, asign its parameters to those in frontend
+          if (response.tarea) {
+            this.tarea = response.tarea;
+            this.titulo = this.tarea.titulo;
+            this.estado = this.tarea.estado;
+            this.fecha = this.tarea.fecha;
+          }
           else this.router.navigate(['/tareas']);
         }, error => {
           this.router.navigate(['/tareas']);
@@ -36,6 +45,9 @@ export class EditTareaComponent implements OnInit {
   }
 
   onSubmit() {
+    this.tarea.titulo = this.titulo;
+    this.tarea.estado = this.estado;
+    this.tarea.fecha = this.fecha;
     this.tareaService.updateTarea(this.tarea)
       .subscribe( response => {
 
